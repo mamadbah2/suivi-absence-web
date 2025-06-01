37b4477909937e27bdb0bc1c99873d7dad96eebf
mamadbah2 <bahmamadoubobosewa@gmail.com>
2025-06-01 03:52:52 +0000

    merger Fabie + bugs fix

--- src/app/page/security/security.component.ts
+++ src/app/page/security/security.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import {Router, RouterModule, RouterOutlet} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { login, logout, isAuthenticated, currentUser } from '../../shared/store/auth.store';
+
 
 @Component({
   selector: 'app-security',
-  imports: [],
   templateUrl: './security.component.html',
-  styleUrl: './security.component.css'
+  styleUrl: './security.component.css',
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    RouterOutlet
+  ]
 })
 export class SecurityComponent {
+  username = '';
+  password = '';
+  loginError = false;
 
+  constructor(private router: Router) {}
+
+  get isLoggedIn() {
+    return isAuthenticated();
+  }
+
+  onLogin() {
+    const success = login(this.username, this.password);
+    this.loginError = !success;
+
+    if (success) {
+      this.router.navigate(['/admin/dashboard']);
+    }
+  }
+
+  onLogout() {
+    logout();
+    this.router.navigate(['/admin']);
+  }
+
+  protected readonly currentUser = currentUser;
 }
